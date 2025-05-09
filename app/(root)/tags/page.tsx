2403aab470f29e9ae54c075873913b5c2e8b8e90
Nafiad <111278128+NafiadAbdi@users.noreply.github.com>
2025-05-09 06:46:15 -0500

    Display Specific Tag Question

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,3 +1,4 @@
+
 import TagCard from "@/components/cards/TagCard";
 import DataRenderer from "@/components/DataRender";
 import LocalSearch from "@/components/search/LocalSearch";
@@ -7,45 +8,45 @@ import { getTags } from "@/lib/actions/tag.action";
 import { RouteParams } from "@/types/global";
 
 const Tags = async ({ searchParams }: RouteParams) => {
-	const { page, pageSize, query, filter } = await searchParams;
+  const { page, pageSize, query, filter } = await searchParams;
 
-	const { success, data, error } = await getTags({
-		page: Number(page) || 1,
-		pageSize: Number(pageSize) || 10,
-		query,
-		filter,
-	});
+  const { success, data, error } = await getTags({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
+  });
 
-	const { tags } = data || {};
+  const { tags } = data || {};
 
-	return (
-		<>
-			<h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900 text-3xl">Tags</h1>
 
-			<section className="mt-11">
-				<LocalSearch
-					route={ROUTES.TAGS}
-					imgSrc="/icons/search.svg"
-					placeholder="Search tags..."
-					otherClasses="flex-1"
-				/>
-			</section>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.TAGS}
+          imgSrc="/icons/search.svg"
+          placeholder="Search tags..."
+          otherClasses="flex-1"
+        />
+      </section>
 
-			<DataRenderer
-				success={success}
-				error={error}
-				data={tags}
-				empty={EMPTY_TAGS}
-				render={(tags) => (
-					<div className="mt-10 flex w-full flex-wrap gap-4">
-						{tags.map((tag) => (
-							<TagCard key={tag._id} {...tag} />
-						))}
-					</div>
-				)}
-			/>
-		</>
-	);
+      <DataRenderer
+        success={success}
+        error={error}
+        data={tags}
+        empty={EMPTY_TAGS}
+        render={(tags) => (
+          <div className="mt-10 flex w-full flex-wrap gap-4">
+            {tags.map((tag) => (
+              <TagCard key={tag._id} {...tag} />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
 };
 
 export default Tags;
--- lib/handlers/action.ts
+++ lib/handlers/action.ts
@@ -1,28 +1,29 @@
 "use server";
 
-import { ZodError, ZodSchema } from "zod";
-import { UnauthorizedError, ValidationError } from "../http-errors";
 import { Session } from "next-auth";
+import { ZodError, ZodSchema } from "zod";
+
 import { auth } from "@/auth";
+
+import { UnauthorizedError, ValidationError } from "../http-errors";
 import dbConnect from "../mongoose";
 
-type ActionsOptions<T> = {
+type ActionOptions<T> = {
 	params?: T;
 	schema?: ZodSchema<T>;
 	authorize?: boolean;
 };
 
-// 1. Checking whether the schema and params are provided and validated
-// 2. Checking wether the user is authorized or not
-// 3. Connecting to the database
-// 4. Returning the params and session if everything is fine
-
+// 1. Checking whether the schema and params are provided and validated.
+// 2. Checking whether the user is authorized.
+// 3. Connecting to the database.
+// 4. Returning the params and session.
 
 async function action<T>({
 	params,
 	schema,
 	authorize = false,
-}: ActionsOptions<T>) {
+}: ActionOptions<T>) {
 	if (schema && params) {
 		try {
 			schema.parse(params);
@@ -38,13 +39,17 @@ async function action<T>({
 	}
 
 	let session: Session | null = null;
+
 	if (authorize) {
 		session = await auth();
+
 		if (!session) {
 			return new UnauthorizedError();
 		}
 	}
+
 	await dbConnect();
+
 	return { params, session };
 }
 
