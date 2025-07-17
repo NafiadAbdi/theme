d993decdf9955f1fcd261dc5a579872b989ccb46
Nafiad <111278128+NafiadAbdi@users.noreply.github.com>
2025-07-17 13:22:21 -0500

    Implement Users Questions

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 
 import UserAvatar from "@/components/UserAvatar";
-import { getUser } from "@/lib/actions/user.action";
+import { getUser, getUserQuestions } from "@/lib/actions/user.action";
 import { notFound } from "next/navigation";
 import dayjs from "dayjs";
 import { Button } from "@/components/ui/button";
@@ -10,120 +10,157 @@ import { RouteParams } from "@/types/global";
 import ProfileLink from "@/components/user/ProfileLink";
 import Stats from "@/components/user/Stats";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import DataRenderer from "@/components/DataRender";
+import { EMPTY_QUESTION } from "@/constants/state";
+import QuestionCard from "@/components/cards/QuestionCard";
+import Pagination from "@/components/Pagination";
 
-const Profile = async ({ params }: RouteParams) => {
-	const { id } = await params;
+const Profile = async ({ params, searchParams }: RouteParams) => {
+  // /12312313
+  const { id } = await params;
+  // ?id=1&page=1&pageSize=10
+  const { page, pageSize } = await searchParams;
 
-	if (!id) notFound();
+  if (!id) notFound();
 
-	const loggedInUser = await auth();
-	const { success, data, error } = await getUser({
-		userId: id,
-	});
+  const loggedInUser = await auth();
+  const { success, data, error } = await getUser({
+    userId: id,
+  });
 
-	if (!success)
-		return (
-			<div>
-				<div className="h1-bold text-dark100_light900">{error?.message}</div>
-			</div>
-		);
+  if (!success)
+    return (
+      <div>
+        <div className="h1-bold text-dark100_light900">{error?.message}</div>
+      </div>
+    );
 
-	const { user, totalQuestions, totalAnswers } = data!;
-	const { _id, name, image, portfolio, location, createdAt, username, bio } =
-		user;
+  const { user, totalQuestions, totalAnswers } = data!;
 
-	return (
-		<>
-			<section className="flex flex-col-reverse items-start justify-between sm:flex-row">
-				<div className="flex flex-col items-start gap-4 lg:flex-row">
-					<UserAvatar
-						id={_id}
-						name={name}
-						imageUrl={image}
-						className="size-[140px] rounded-full object-cover"
-						fallbackClassName="text-6xl fond-bolder"
-					/>
+  const {
+    success: userQuestionsSuccess,
+    data: userQuestions,
+    error: userQuestionsError,
+  } = await getUserQuestions({
+    userId: id,
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+  });
 
-					<div className="mt-3">
-						<h2 className="h2-bold text-dark100_light900">{name}</h2>
-						<p className="paragraph-regular text-dark200_light800">
-							@{username}
-						</p>
+  const { questions, isNext: hasMoreQuestions } = userQuestions!;
 
-						<div className="mt-5 flex flex-wrap items-center justify-start gap-5">
-							{portfolio && (
-								<ProfileLink
-									imgUrl="/icons/link.svg"
-									href={portfolio}
-									title="Portfolio"
-								/>
-							)}
-							{location && (
-								<ProfileLink imgUrl="/icons/location.svg" title="Portfolio" />
-							)}
-							<ProfileLink
-								imgUrl="/icons/calendar.svg"
-								title={dayjs(createdAt).format("MMMM YYYY")}
-							/>
-						</div>
+  const { _id, name, image, portfolio, location, createdAt, username, bio } =
+    user;
 
-						{bio && (
-							<p className="paragraph-regular text-dark400_light800 mt-8">
-								{bio}
-							</p>
-						)}
-					</div>
-				</div>
+  return (
+    <>
+      <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+        <div className="flex flex-col items-start gap-4 lg:flex-row">
+          <UserAvatar
+            id={_id}
+            name={name}
+            imageUrl={image}
+            className="size-[140px] rounded-full object-cover"
+            fallbackClassName="text-6xl fond-bolder"
+          />
 
-				<div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
-					{loggedInUser?.user?.id === id && (
-						<Link href="/profile/edit">
-							<Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
-								Edit Profile
-							</Button>
-						</Link>
-					)}
-				</div>
-			</section>
-			<Stats
-				totalQuestions={totalQuestions}
-				totalAnswers={totalAnswers}
-				badges={{
-					GOLD: 0,
-					SILVER: 0,
-					BRONZE: 0,
-				}}
-			/>
-			<section className="mt-10 flex gap-10">
-				<Tabs defaultValue="top-posts" className="flex-[2]">
-					<TabsList className="background-light800_dark400 min-h-[42px] p-1">
-						<TabsTrigger value="top-posts" className="tab">
-							Top Posts
-						</TabsTrigger>
-						<TabsTrigger value="answers" className="tab">
-							Answers
-						</TabsTrigger>
-					</TabsList>
-					<TabsContent
-						value="top-posts"
-						className="mt-5 flex w-full flex-col gap-6"
-					>
-						List of Questions
-					</TabsContent>
-					<TabsContent value="answers" className="flex w-full flex-col gap-6">
-						List of Answers
-					</TabsContent>
-				</Tabs>
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{name}</h2>
+            <p className="paragraph-regular text-dark200_light800">
+              @{username}
+            </p>
 
-				<div className="flex w-full min-w-[250px] flex-1 flex-col max-lg:hidden">
-					<h3 className="h3-bold text-dark200_light900">Top Tech</h3>
-					<div className="mt-7 flex flex-col gap-4">
-						<p>List of Tags</p>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {portfolio && (
+                <ProfileLink
+                  imgUrl="/icons/link.svg"
+                  href={portfolio}
+                  title="Portfolio"
+                />
+              )}
+              {location && (
+                <ProfileLink imgUrl="/icons/location.svg" title="Portfolio" />
+              )}
+              <ProfileLink
+                imgUrl="/icons/calendar.svg"
+                title={dayjs(createdAt).format("MMMM YYYY")}
+              />
+            </div>
+
+            {bio && (
+              <p className="paragraph-regular text-dark400_light800 mt-8">
+                {bio}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+          {loggedInUser?.user?.id === id && (
+            <Link href="/profile/edit">
+              <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
+                Edit Profile
+              </Button>
+            </Link>
+          )}
+        </div>
+      </section>
+
+      <Stats
+        totalQuestions={totalQuestions}
+        totalAnswers={totalAnswers}
+        badges={{
+          GOLD: 0,
+          SILVER: 0,
+          BRONZE: 0,
+        }}
+      />
+
+      <section className="mt-10 flex gap-10">
+        <Tabs defaultValue="top-posts" className="flex-[2]">
+          <TabsList className="background-light800_dark400 min-h-[42px] p-1">
+            <TabsTrigger value="top-posts" className="tab">
+              Top Posts
+            </TabsTrigger>
+            <TabsTrigger value="answers" className="tab">
+              Answers
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent
+            value="top-posts"
+            className="mt-5 flex w-full flex-col gap-6"
+          >
+            <DataRenderer
+              data={questions}
+              empty={EMPTY_QUESTION}
+              success={userQuestionsSuccess}
+              error={userQuestionsError}
+              render={(hotQuestions) => (
+                <div className="flex w-full flex-col gap-6">
+                  {questions.map((question) => (
+                    <QuestionCard key={question._id} question={question} />
+                  ))}
+                </div>
+              )}
+            />
+
+            <Pagination page={page} isNext={hasMoreQuestions} />
+          </TabsContent>
+
+          <TabsContent value="answers" className="flex w-full flex-col gap-6">
+            List of Answers
+          </TabsContent>
+        </Tabs>
+
+        <div className="flex w-full min-w-[250px] flex-1 flex-col max-lg:hidden">
+          <h3 className="h3-bold text-dark200_light900">Top Tech</h3>
+          <div className="mt-7 flex flex-col gap-4">
+            <p>List of Tags</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Profile;
